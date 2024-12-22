7243374fc661f50b822f61b88873b5db49595b70
ejoyee <whdmsdl401@gmail.com>
2024-12-22 21:03:11 +0900

    FIX : handle 변수명 통일

--- time-service/src/main/java/mockcote/timeservice/service/TimeService.java
+++ time-service/src/main/java/mockcote/timeservice/service/TimeService.java
@@ -5,7 +5,7 @@ import mockcote.timeservice.dto.SubmissionRequest;
 public interface TimeService {
 
   	// 풀이여부체크
-    String checkSubmissionStatus(String userId, Integer problemId) throws Exception;
+    String checkSubmissionStatus(String handle, Integer problemId) throws Exception;
   
     /**
      * 풀이 로그 저장
