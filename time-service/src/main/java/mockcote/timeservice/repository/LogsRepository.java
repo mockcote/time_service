9983df9bd4875132df361281fe5460a23501a22e
3957ki <3957ki@gmail.com>
2024-12-29 15:09:44 +0900

    FIX : application.yml 수정

--- time-service/src/main/java/mockcote/timeservice/repository/LogsRepository.java
+++ time-service/src/main/java/mockcote/timeservice/repository/LogsRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LogsRepository extends JpaRepository<Logs, Long> {
-    // 추가적으로 사용자 ID나 문제 ID별로 조회할 메서드를 선언할 수 있습니다.
-    // 예시: List<Logs> findByUserId(Integer userId);
 }
