976049a8749a597f2fb56716a3d5c71b55d7c7eb
3957ki <3957ki@gmail.com>
2024-12-30 19:22:54 +0900

    Revert "FEAT : 문제 풀이 시작, 종료 api 작성"
    This reverts commit 8b4f3c86adc97e1f1437c4b8f8128e6a4957ad4d.

--- time-service/src/main/java/mockcote/timeservice/service/TimeService.java
+++ time-service/src/main/java/mockcote/timeservice/service/TimeService.java
@@ -1,24 +1,8 @@
 package mockcote.timeservice.service;
 
-import jakarta.validation.constraints.NotBlank;
 import mockcote.timeservice.dto.SubmissionRequest;
 
-import java.time.LocalDateTime;
-
 public interface TimeService {
-
-    /**
-     * 풀이 시작
-     * @param handle
-     * @param now
-     */
-    LocalDateTime timeStart(@NotBlank String handle, LocalDateTime now);
-
-    /**
-     * 풀이 종료
-     * @param handle
-     */
-    void timeEnd(@NotBlank String handle);
 
     /**
      * 풀이 여부 체크
@@ -34,4 +18,5 @@ public interface TimeService {
      * @return
      */
     String saveLog(SubmissionRequest request);
+
 }
